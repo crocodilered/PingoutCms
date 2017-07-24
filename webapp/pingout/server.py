755f076669b2311547dd7c7f30c55b4143cead9f
Serge Zolotukhin <serge.zolotukhin@gmail.com>
2017-07-24 14:32:26 +0300

    ' to "

--- webapp/pingout/server.py
+++ webapp/pingout/server.py
@@ -8,7 +8,7 @@ from requests.adapters import HTTPAdapter
 from urllib3 import PoolManager
 
 
-__all__ = ['Server']
+__all__ = ["Server"]
 
 
 class Server:
@@ -30,24 +30,24 @@ class Server:
     def _read_socket(self):
         packet_header = self.connection.recv(8)
         request_header = self.connection.recv(5)
-        body_size, crc, type, code = struct.unpack('<LLBL', packet_header + request_header)
-        r = b''
+        body_size, crc, type, code = struct.unpack("<LLBL", packet_header + request_header)
+        r = b""
         while len(r) < body_size:
             chunk = self.connection.recv(body_size - len(r))
-            if chunk == '':
+            if chunk == "":
                 break
             r += chunk
-        if crcmod.predefined.mkCrcFun('crc-32c')(request_header + r) != crc:
+        if crcmod.predefined.mkCrcFun("crc-32c")(request_header + r) != crc:
             return 1, None
-        return code, json.loads(r.decode('utf-8'))
+        return code, json.loads(r.decode("utf-8"))
 
     def _write_socket(self, data):
         try:
-            data_json = json.dumps(data).encode('utf-8')
-            request_header = struct.pack('<BL', 1, 2)
-            packet_header = struct.pack('<LL',
+            data_json = json.dumps(data).encode("utf-8")
+            request_header = struct.pack("<BL", 1, 2)
+            packet_header = struct.pack("<LL",
                                         len(data_json),
-                                        crcmod.predefined.mkCrcFun('crc-32c')(request_header + data_json))
+                                        crcmod.predefined.mkCrcFun("crc-32c")(request_header + data_json))
             self.connection.sendall(packet_header + request_header + data_json)
             while True:
                 code, r = self._read_socket()
@@ -63,9 +63,9 @@ class Server:
 
     #  PUBLIC  #########################################################################################################
 
-    def action(self, cmd, token='', args=None):
+    def action(self, cmd, token="", args=None):
         data = [cmd]
-        if cmd not in ['send_otp', 'validate_otp']:
+        if cmd not in ["send_otp", "validate_otp"]:
             data += [token]
         # print(data + [args])
         resp = self._write_socket(data + [args])
@@ -73,11 +73,11 @@ class Server:
 
     def upload_file(self, token, file_data):
         session = requests.Session()
-        session.mount('https://', Tls12HttpAdapter())
+        session.mount("https://", Tls12HttpAdapter())
         file_name = None
         files = {
-            'file': ('file', file_data, 'image/jpeg'),
-            'json': ('file', json.dumps(['upload', token]), 'application/json')
+            "file": ("file", file_data, "image/jpeg"),
+            "json": ("file", json.dumps(["upload", token]), "application/json")
         }
         r = session.post("https://%s:%s/" % (self._server_host, self._server_https_port),
                          files=files, verify=self._server_cert)
