c2b17558085eba59ac960401439ea7a248a1ea0f
Serge Zolotukhin <serge.zolotukhin@gmail.com>
2017-08-11 11:00:57 +0300

    Comments added.

--- webapp/pingout/point.py
+++ webapp/pingout/point.py
@@ -5,7 +5,11 @@ __all__ = ['Coordinates']
 
 
 class Point(object):
-
+    """
+    Transforming internal coordinates system to geo.
+    Point - internal term
+    Location - geo term
+    """
     def __init__(self, x, y):
         self.x = x
         self.y = y
@@ -18,6 +22,12 @@ class Point(object):
         self.LONGITUDE_MAX = 180.0
 
     def from_location(self, lon, lat):
+        """
+        Another constructor
+        :param lon: longitude
+        :param lat: latitude
+        :return:
+        """
         if lat < -self.LATITUDE_MAX or lat > self.LATITUDE_MAX or lon < -self.LONGITUDE_MAX or lat > self.LONGITUDE_MAX:
             raise()
         sin_y = math.sin(lat * self.DEG2RAD)
@@ -25,13 +35,26 @@ class Point(object):
         self.y = (0.5 * math.log((1 + sin_y) / (1 - sin_y)) / -self.M2PI + 0.5) * self.WORLD_SIZE
 
     def get_location(self):
+        """
+        Get Geo of Point
+        :return:
+        """
         lng = (self.x / self.WORLD_SIZE - 0.5) * 360
         lat = 90 - 2 * math.atan(math.exp(-(0.5 - (self.y / self.WORLD_SIZE)) * self.M2PI)) * self.RAD2DEG
         return lng, lat
 
     def get_distance(self, point):
+        """
+        Get distance to another Point
+        :param point: another point
+        :return:
+        """
         distance = math.hypot(self.x - point.x, self.y - point.y)
         return distance * self.get_meters_per_pixel() if distance > 1 else 0.0
 
     def get_meters_per_pixel(self):
+        """
+        Get 'resolution'
+        :return:
+        """
         return math.cos(self.get_location()[1] * self.DEG2RAD) * self.M2PI * self.EARTH_RADIUS_M / self.WORLD_SIZE
