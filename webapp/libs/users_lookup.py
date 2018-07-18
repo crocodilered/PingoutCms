a9dcb30660a7520bbcecef2a22b0164a07291538
Serge Zolotukhin <serge.zolotukhin@gmail.com>
2018-07-18 17:48:43 +0300

    Bug with wrong key fixed.

--- webapp/libs/users_lookup.py
+++ webapp/libs/users_lookup.py
@@ -11,7 +11,7 @@ class UsersLookup(AbstractLookup):
     def val(self, user):
         return {
             "user_id":        user["user_id"],
-            "profile_name":   user["profile"]["name"] if "gender" in user["profile"] else "",
+            "profile_name":   user["profile"]["name"] if "name" in user["profile"] else "",
             "profile_gender": user["profile"]["gender"] if "gender" in user["profile"] else "",
             "profile_age":    user["profile"]["age"] if "age" in user["profile"] else "",
             "profile_about":  user["profile"]["about"] if "about" in user["profile"] else "",
